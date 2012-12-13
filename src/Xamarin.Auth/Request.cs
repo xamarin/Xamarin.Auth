077587543c0b28386eb227bf44ad5f1510c33399
ermau <me@ermau.com>
2012-12-12 21:30:57 -0500

    Include requests in XAMARIN_AUTH_INTERNAL

--- src/Xamarin.Auth/Request.cs
+++ src/Xamarin.Auth/Request.cs
@@ -30,7 +30,11 @@ namespace Xamarin.Auth
 	/// An HTTP web request that provides a convenient way to make authenticated
 	/// requests using account objects obtained from an authenticator.
 	/// </summary>
+#if XAMARIN_AUTH_INTERNAL
+	internal class Request
+#else
 	public class Request
+#endif
 	{
 		HttpWebRequest request;
 
