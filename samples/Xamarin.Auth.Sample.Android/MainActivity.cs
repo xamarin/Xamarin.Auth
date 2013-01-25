f441a1aea920b67bf490cb350579eba4edc4a70a
ermau <me@ermau.com>
2013-01-25 16:43:12 -0500

    Improve samples and getting started
    - Remove Skydrive example (it's OAuth2 just like Facebook, we don't need
      two examples of the same thing).
    - Demonstrate using *Request methods to retrieve Facebook name
    - Use Facebook instead of Skydrive in GettingStarted
    - Correct a small issue in iOS sample
    - Add some comments and generally make things clearer

--- samples/Xamarin.Auth.Sample.Android/MainActivity.cs
+++ samples/Xamarin.Auth.Sample.Android/MainActivity.cs
@@ -1,9 +1,7 @@
 using System;
-
+using System.Json;
+using System.Threading.Tasks;
 using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
 using Android.Widget;
 using Android.OS;
 
@@ -12,63 +10,51 @@ namespace Xamarin.Auth.Sample.Android
 	[Activity (Label = "Xamarin.Auth Sample (Android)", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
-		protected override void OnCreate (Bundle bundle)
-		{
-			base.OnCreate (bundle);
-
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
-
-			var facebook = FindViewById<Button> (Resource.Id.FacebookButton);			
-			facebook.Click += LoginToFacebook;
-
-			var skydrive = FindViewById<Button> (Resource.Id.SkydriveButton);
-			skydrive.Click += LoginToSkydrive;
-		}
-
 		void LoginToFacebook (object sender, EventArgs e)
 		{
-			var a = new OAuth2Authenticator (
+			var auth = new OAuth2Authenticator (
 				clientId: "346691492084618",
 				scope: "",
 				authorizeUrl: new Uri ("https://m.facebook.com/dialog/oauth/"),
 				redirectUrl: new Uri ("http://www.facebook.com/connect/login_success.html"));
 
-			a.Completed += (s, ee) => {
-				var tv = FindViewById<TextView> (Resource.Id.FacebookTextView);
-				tv.Text = GetEventAsString (ee);
+			// If authorization succeeds or is canceled, .Completed will be fired.
+			auth.Completed += (s, ee) => {
+				if (!ee.IsAuthenticated) {
+					this.facebookStatus.Text = "Not Authenticated";
+					return;
+				}
+
+				// Now that we're logged in, make a OAuth2 request to get the user's info.
+				var request = new OAuth2Request ("GET", new Uri ("https://graph.facebook.com/me"), null, ee.Account);
+				request.GetResponseAsync().ContinueWith (t => {
+					if (t.IsFaulted)
+						this.facebookStatus.Text = "Error: " + t.Exception.InnerException.Message;
+					else if (t.IsCanceled)
+						this.facebookStatus.Text = "Canceled";
+					else
+					{
+						var obj = JsonValue.Parse (t.Result.GetResponseText());
+						this.facebookStatus.Text = "Logged in as " + obj["name"];
+					}
+				}, uiScheduler);
 			};
 
-			var intent = a.GetUI (this);
+			var intent = auth.GetUI (this);
 			StartActivityForResult (intent, 42);
 		}
 
-		void LoginToSkydrive (object sender, EventArgs e)
+		private TextView facebookStatus;
+		private readonly TaskScheduler uiScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+		protected override void OnCreate (Bundle bundle)
 		{
-			var a = new OAuth2Authenticator (
-				clientId: "00000000440DC040",
-				scope: "wl.basic,wl.share,wl.skydrive",
-				authorizeUrl: new Uri ("https://login.live.com/oauth20_authorize.srf"),
-				redirectUrl: new Uri ("https://login.live.com/oauth20_desktop.srf"));
+			base.OnCreate (bundle);
+			SetContentView (Resource.Layout.Main);
 
-			a.Completed += (s, ee) => {
-				var tv = FindViewById<TextView> (Resource.Id.SkydriveTextView);
-				tv.Text = GetEventAsString (ee);
-			};
-
-			var intent = a.GetUI (this);
-			StartActivityForResult (intent, 42);
-		}
-
-		string GetEventAsString (AuthenticatorCompletedEventArgs e)
-		{
-			if (e.IsAuthenticated) {
-				return e.Account.Serialize ();
-			} else {
-				return "Not Authenticated";
-			}
+			this.facebookStatus = FindViewById<TextView> (Resource.Id.FacebookTextView);
+			var facebook = FindViewById<Button> (Resource.Id.FacebookButton);			
+			facebook.Click += LoginToFacebook;
 		}
 	}
 }
-
-
