4f57a329c37306b30a80fa2b9e40d8d6b81a9849
Kuzya <alexkuz@gmail.com>
2013-05-24 16:15:02 +0400

    OAuth2Authenticator: protected fields

--- src/Xamarin.Auth/OAuth2Authenticator.cs
+++ src/Xamarin.Auth/OAuth2Authenticator.cs
@@ -33,12 +33,12 @@ namespace Xamarin.Auth
 	public class OAuth2Authenticator : WebRedirectAuthenticator
 #endif
 	{
-		string clientId;
-		string clientSecret;
-		string scope;
-		Uri authorizeUrl;
-		Uri redirectUrl;
-		Uri accessTokenUrl;
+		protected string clientId;
+		protected string clientSecret;
+		protected string scope;
+		protected Uri authorizeUrl;
+		protected Uri redirectUrl;
+		protected Uri accessTokenUrl;
 		GetUsernameAsyncFunc getUsernameAsync;
 		ResponseFormat tokenResponseFormat;
 
