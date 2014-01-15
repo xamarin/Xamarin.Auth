ab535cb63a62400f01242330de93519f6025fd67
sanetby <anton.makarevich@gmail.com>
2014-01-15 19:56:28 +0300

    [windows 8] added support for gamepages
    DirectX and monogame projects are using pages with
    SwapChainBackgroundPanel as root visual element and don't support Frame
    navigation, so need to set content manually

--- src/Xamarin.Auth.Store/AuthPage.xaml.cs
+++ src/Xamarin.Auth.Store/AuthPage.xaml.cs
@@ -36,23 +36,36 @@ namespace Xamarin.Auth.Store
         /// </summary>
         /// <param name="e">Event data that describes how this page was reached.  The Parameter
         /// property is typically used to configure the page.</param>
-        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            _auth = e.Parameter as WebAuthenticator;
-                _auth.Completed += (sender, args) => 
+            SetAuth(e.Parameter as WebAuthenticator);
+        }
+
+        /// <summary>
+        /// pages with SwapChainBackgroundPanel don't support frame navigation
+        /// so need this to manually start process
+        /// </summary>
+        /// <param name="auth"></param>
+        public async void SetAuth(WebAuthenticator auth)
+        {
+            _auth = auth;
+            _auth.Completed += (sender, args) =>
+            {
+                try
                 {
+                    //try to manually go back, might not work based on navigation method
                     this.Frame.GoBack();
-                };
-			_auth.Error+=_auth_Error;
+                }
+                catch { }
+            };
+            _auth.Error += _auth_Error;
             this.progress.IsActive = true;
             Uri uri = await _auth.GetInitialUrlAsync();
             browser.Source = uri;
             browser.Navigate(uri);
-            
+
             _auth.OnPageLoading(uri);
         }
-
-        
         
         void _auth_Error(object sender, AuthenticatorErrorEventArgs e)
         {
