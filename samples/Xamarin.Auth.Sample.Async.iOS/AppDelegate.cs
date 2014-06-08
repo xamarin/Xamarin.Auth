66895c749183a73e9ddfd8e855070a72ad6feb7e
Sara Silva <saramgsilva@gmail.com>
2014-06-08 12:12:12 +0100

    Add sample for IOS using async

---
+++ samples/Xamarin.Auth.Sample.Async.iOS/AppDelegate.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.Dialog;
+using Xamarin.Auth_Async_Sample;
+
+namespace Xamarin.Auth_Async_Sample__IOS_
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        async void LoginToFacebook(bool allowCancel)
+        {
+            var facebookService = new FacebookService();
+            var loginResult = await facebookService.LoginAsync(dialog, allowCancel);
+
+            if (!loginResult.IsAuthenticated)
+            {
+                ShowMessage("Not Authenticated");
+                return;
+            }
+
+            // in this step the username is always null, but i followed the original sample
+            ShowMessage(string.Format("Authenticated {0}", loginResult.Account.Username));
+
+            var userInfo = await facebookService.GetUserInfoAsync(loginResult.Account);
+            ShowMessage(!string.IsNullOrEmpty(userInfo) ? string.Format("Logged as {0}", userInfo) : "Wasn´t possible to get the name.");
+       }
+
+        private void ShowMessage(string message)
+        {
+            facebookStatus.Caption = message;
+            dialog.ReloadData();
+        }
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            facebook = new Section("Facebook")
+            {
+                new StyledStringElement("Log in", () => LoginToFacebook(true)),
+                new StyledStringElement("Log in (no cancel)", () => LoginToFacebook(false))
+            };
+            facebook.Add(facebookStatus = new StringElement(String.Empty));
+
+            dialog = new DialogViewController(new RootElement("Xamarin.Auth Async Sample") {
+				facebook,
+			});
+
+            window = new UIWindow(UIScreen.MainScreen.Bounds)
+            {
+                RootViewController = new UINavigationController(dialog)
+            };
+            window.MakeKeyAndVisible();
+
+            return true;
+        }
+
+        UIWindow window;
+        DialogViewController dialog;
+
+        Section facebook;
+        StringElement facebookStatus;
+    }
+}
---
+++ samples/Xamarin.Auth.Sample.Async.iOS/FacebookService.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Json;
+using System.Threading.Tasks;
+using MonoTouch.Dialog;
+using MonoTouch.UIKit;
+using Xamarin.Auth;
+
+namespace Xamarin.Auth_Async_Sample
+{
+    public class FacebookService
+    {
+        public async Task<AuthenticatorCompletedEventArgs> LoginAsync(DialogViewController dialog, bool allowCancel)
+        {
+            var auth = new OAuth2Authenticator(
+                clientId: "497156763693780",
+                scope: "basic_info, public_profile, email",
+                authorizeUrl: new Uri("https://m.facebook.com/dialog/oauth/"),
+                redirectUrl: new Uri("https://www.facebook.com/connect/login_success.html"))
+            {
+                AllowCancel = allowCancel
+            };
+
+            // If authorization succeeds or is canceled, .Completed will be fired.
+            var tcs1 = new TaskCompletionSource<AuthenticatorCompletedEventArgs>();
+            EventHandler<AuthenticatorCompletedEventArgs> d1 =
+                (o, e) =>
+                {
+                    try
+                    {
+                        tcs1.TrySetResult(e);
+                    }
+                    catch (Exception ex)
+                    {
+                        tcs1.TrySetResult(new AuthenticatorCompletedEventArgs(null));
+                    }
+                };
+            auth.Completed += d1;
+            UIViewController vc = auth.GetUI();
+            dialog.PresentViewController(vc, true, null);
+            var result= await tcs1.Task;
+            auth.Completed -= d1;
+            return result;
+        }
+
+        public async Task<string> GetUserInfoAsync(Account account)
+        {
+            try
+            {
+                var request = new OAuth2Request("GET", new Uri("https://graph.facebook.com/me"), null, account);
+                var userInfoResult = await request.GetResponseAsync();
+                var obj = JsonValue.Parse(userInfoResult.GetResponseText());
+                if (obj != null)
+                {
+                    // the best solution is to create an object with all parameters, but it is an example :)
+                    return obj["name"];
+                }
+            }
+            catch (Exception ex)
+            {
+                //Todo
+            }
+            return null;
+        }
+    }
+}
