aacfa2d7f0bbdb4ccbbf73860eaef0f4c7fe6400
moljac <mcvjetko@holisticware.net>
2016-02-09 12:55:57 +0100

    try/catch added to solve   Android.Views.WindowManagerBadTokenException:   Unable to add window -- token android.os.BinderProxy@52907c38 is not valid;   is your activity running? https://bugzilla.xamarin.com/show_bug.cgi?id=37870				https://forums.xamarin.com/discussion/11491/xamarin-auth-alway-throws-exception-on-android

--- source/Xamarin.Auth.XamarinAndroid/PlatformSpecific/ActivityEx.cs
+++ source/Xamarin.Auth.XamarinAndroid/PlatformSpecific/ActivityEx.cs
@@ -16,14 +16,32 @@ namespace Xamarin.Utilities.Android
 
 		public static void ShowError (this Activity activity, string title, string message)
 		{
-			var b = new AlertDialog.Builder (activity);
-			b.SetMessage (message);
-			b.SetTitle (title);
-			b.SetNeutralButton ("OK", (s, e) => {
-				((AlertDialog)s).Cancel ();
-			});
-			var alert = b.Create ();
-			alert.Show ();
+			/*
+			 	try/catch added to solve
+				Android.Views.WindowManagerBadTokenException: 
+					Unable to add window -- token android.os.BinderProxy@52907c38 is not valid; 
+					is your activity running?
+
+				https://bugzilla.xamarin.com/show_bug.cgi?id=37870
+				https://forums.xamarin.com/discussion/11491/xamarin-auth-alway-throws-exception-on-android
+			*/
+			try
+			{
+				var b = new AlertDialog.Builder (activity);
+				b.SetMessage (message);
+				b.SetTitle (title);
+				b.SetNeutralButton ("OK", (s, e) => {
+					((AlertDialog)s).Cancel ();
+				});
+				var alert = b.Create ();
+				alert.Show ();
+			}
+			catch (Exception ex) 
+			{
+				global::Android.Util.Log.Error("Xamarin.Auth", "Error: {0}:{1}", title, message);
+			}
+
+			return;
 		}
 	}
 }
