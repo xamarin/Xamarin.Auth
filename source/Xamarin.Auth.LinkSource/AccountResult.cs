1f0efdb310bb5afe621d237089e754578ab80dad
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-04-17 08:58:13 +0200

    comment about PR fixed

--- source/Xamarin.Auth.LinkSource/AccountResult.cs
+++ source/Xamarin.Auth.LinkSource/AccountResult.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 namespace Xamarin.Auth
 {
-	// Pull Request - manually added/fixed
-	//		Marshalled NavigationService.GoBack to UI Thread #94
-	//		https://github.com/xamarin/Xamarin.Auth/pull/88
-	public partial class AccountResult
+    // Pull Request - manually added/fixed
+    //		Added IsAuthenticated check #88
+    //		https://github.com/xamarin/Xamarin.Auth/pull/88
+    public partial class AccountResult
 	{
 		public string Name { get; set; }
 		public string AccountType { get; set; }
