9277a22cdc150430fae26b871df47cd6698f3e75
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-04-26 09:40:42 -0400

    samples based on nuget references

---
+++ samples/Traditional.Standard/references02nuget/Xamarin.Auth.Sample.WindowsPhone8/SampleData/Providers/Data.Instagram.OAuth2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Auth.Helpers;
+
+namespace Xamarin.Auth.SampleData
+{
+	public partial class Data 
+	{
+		static partial void SetPublicDemoDataInstagramOAuth2()
+		{
+			oauth2 = new Xamarin.Auth.Helpers.OAuth2()
+			{
+				Description = "Instagram OAuth2",
+				OAuth_IdApplication_IdAPI_KeyAPI_IdClient_IdCustomer = "",
+				OAuth2_Scope = "basic",
+				OAuth_UriAuthorization = new Uri("https://api.instagram.com/oauth/authorize/"), 
+				OAuth_UriCallbackAKARedirect = new Uri("http://xamarin.com"),
+				AllowCancel = true,
+			};
+
+			if (TestCases.ContainsKey(oauth2.Description))
+			{
+				TestCases[oauth2.Description] = oauth2;
+			}
+			else
+			{
+				TestCases.Add(oauth2.Description, oauth2);
+			}
+
+			return;
+		}
+	}
+}
+
---
+++ samples/Traditional.Standard/references02nuget/Xamarin.Auth.Sample.WindowsPhone8/SampleData/Providers/Data.LinkedIn.OAuth1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Xamarin.Auth.SampleData
+{
+	public partial class Data 
+	{
+		static partial void SetPublicDemoDataLinkedInOAuth1()
+		{
+			oauth1 = new Xamarin.Auth.Helpers.OAuth1()
+			{
+				Description = "LinkedIn OAuth1",
+				OAuth_IdApplication_IdAPI_KeyAPI_IdClient_IdCustomer = "",
+				OAuth1_SecretKey_ConsumerSecret_APISecret = "",
+				OAuth1_UriRequestToken = new Uri("https://api.linkedin.com/uas/oauth/requestToken"),
+				OAuth_UriAuthorization = new Uri("https://api.linkedin.com/uas/oauth/authorize"), 
+				OAuth_UriCallbackAKARedirect = new Uri("http://xamarin.com"),
+				OAuth1_UriAccessToken = new Uri("https://api.linkedin.com/uas/oauth/accessToken"),
+				AllowCancel = true,
+			};
+
+			if (TestCases.ContainsKey(oauth1.Description))
+			{
+				TestCases[oauth1.Description] = oauth1;
+			}
+			else
+			{
+				TestCases.Add(oauth1.Description, oauth1);
+			}
+
+			return;
+		}
+	}
+}
+
