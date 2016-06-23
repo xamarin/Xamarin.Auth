3c221383e4ee5c5e4ec01bd3e84297a46afbaaef
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-06-23 19:04:39 +0200

    samples for providers GitLab update Slack added

---
+++ samples/Xamarin.Auth.SampleData/SampleData/Providers/GitLabOAuth2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Xamarin.Auth.SampleData
+{
+	public partial class GitLabOAuth2 : Xamarin.Auth.Helpers.OAuth2
+	{
+		partial void SetPublicNonSensitiveData();
+		partial void SetPrivateSensitiveData();
+
+		public GitLabOAuth2()
+		{
+			SetPublicNonSensitiveData();
+			SetPrivateSensitiveData();
+
+			return;
+		}
+
+		partial void SetPublicNonSensitiveData()
+		{
+			HowToMarkDown =
+            @"
+http://doc.gitlab.com/ce/integration/oauth_provider.html
+
+https://gitlab.com/oauth/authorize
+
+			";
+			Description = "Gitlab OAuth2";
+			OAuth_IdApplication_IdAPI_KeyAPI_IdClient_IdCustomer = "";
+			OAuth2_Scope = "api"; // 
+			OAuth_UriAuthorization = new Uri("https://gitlab.com/oauth/authorize");
+			OAuth_UriCallbackAKARedirect = new Uri("https://xamarin.com");
+			AllowCancel = true;
+						
+			return;
+		}
+
+	}
+}
+
