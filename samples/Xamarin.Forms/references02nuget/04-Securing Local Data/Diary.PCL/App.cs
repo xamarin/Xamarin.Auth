0a838f8df96cd35f17378f1c635d1e1fe440aace
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-06-27 15:07:00 +0200

    Evolve16 Labs sample added Securing Local Data

---
+++ samples/Xamarin.Forms/references02nuget/04-Securing Local Data/Diary.PCL/App.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Diary
+{
+	public class App : Application
+	{
+		public static string DatabaseFolder { get; set; }
+
+		static DiaryEntryStore store;
+		public static DiaryEntryStore Store
+		{
+			get
+			{
+				return store != null ? store : store = new DiaryEntryStore(DatabaseFolder, "DiaryEntries.db3");
+			}
+		}
+
+		public App()
+		{
+			var loginPage = new LoginPage ();
+			loginPage.LoginSucceeded += (account) =>  MainPage = new NavigationPage (new DiaryEntriesPage (account));
+
+			MainPage = new NavigationPage (loginPage);
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
---
+++ samples/Xamarin.Forms/references02nuget/04-Securing Local Data/Diary.PCL/Security/CryptoUtilities.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using PCLCrypto;
+using System.Text;
+
+namespace Diary.Shared
+{
+	public static class CryptoUtilities
+	{
+		const int IVSize = 16;
+
+		public static byte[] GetAES256KeyMaterial ()
+		{
+			return WinRTCrypto.CryptographicBuffer.GenerateRandom(32); 
+		}
+
+		public static byte[] Get256BitSalt ()
+		{
+			return WinRTCrypto.CryptographicBuffer.GenerateRandom(32); 
+		}
+
+		public static byte[] Encrypt (byte[] plainText, byte[] keyMaterial)
+		{
+            var provider = WinRTCrypto.SymmetricKeyAlgorithmProvider
+                .OpenAlgorithm(SymmetricAlgorithm.AesCbcPkcs7);
+
+            var key = provider.CreateSymmetricKey(keyMaterial);
+            var IV = WinRTCrypto.CryptographicBuffer.GenerateRandom(IVSize);
+            var cipher = WinRTCrypto.CryptographicEngine.Encrypt(key, plainText, IV);
+                
+            var cipherText = new byte[IV.Length + cipher.Length];
+
+            IV.CopyTo(cipherText, 0);
+            cipher.CopyTo(cipherText, IV.Length);
+
+			return cipherText;
+		}
+
+		public static byte[] Decrypt (byte[] cipherText, byte[] keyMaterial)
+		{
+            var provider = WinRTCrypto.SymmetricKeyAlgorithmProvider
+                .OpenAlgorithm(SymmetricAlgorithm.AesCbcPkcs7);
+
+            var key = provider.CreateSymmetricKey(keyMaterial);
+
+            byte[] IV = new byte[IVSize];
+            Array.Copy(cipherText, IV, IV.Length);
+
+            byte[] cipher = new byte[cipherText.Length - IVSize];
+            Array.Copy(cipherText, IVSize, cipher, 0, cipher.Length);
+
+            return WinRTCrypto.CryptographicEngine.Decrypt(key, cipher, IV);
+		}
+
+		public static byte[] GetHash (byte[] data, byte[] salt)
+		{
+            byte[] saltedData = new byte[data.Length + salt.Length];
+            Array.Copy(salt, saltedData, salt.Length);
+            Array.Copy(data, 0, saltedData, salt.Length, data.Length);
+
+            var sha = WinRTCrypto.HashAlgorithmProvider.OpenAlgorithm(HashAlgorithm.Sha256);
+            return sha.HashData(saltedData);
+		}
+
+		public static string ByteArrayToString (byte[] data)
+		{
+			return Encoding.UTF8.GetString (data, 0, data.Length);
+		}
+
+		public static byte[] StringToByteArray (string text)
+		{
+			return Encoding.UTF8.GetBytes (text);
+		}
+	}
+}
