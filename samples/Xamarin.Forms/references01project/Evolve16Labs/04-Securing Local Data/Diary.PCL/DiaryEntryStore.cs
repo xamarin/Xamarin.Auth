8a0eb7d2ebaa886d7f81cc3e434238e5078b2507
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-06-27 10:46:08 +0200

    Evovle16 Securing Local Data sample

---
+++ samples/Xamarin.Forms/references01project/Evolve16Labs/04-Securing Local Data/Diary.PCL/DiaryEntryStore.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using SQLite;
+using System.Linq;
+
+namespace Diary
+{
+	public class DiaryEntryStore
+	{
+		string path;
+		SQLiteAsyncConnection connection;
+
+		public DiaryEntryStore(string folder, string filename)
+		{
+			path = System.IO.Path.Combine(folder, filename);
+
+			connection = new SQLiteAsyncConnection(path);
+
+			connection.CreateTableAsync<DiaryEntry>().Wait();
+		}
+
+		public Task SaveEntryAsync(DiaryEntry entry)
+		{
+			if (entry.Id == -1)
+				return connection.InsertAsync(entry);
+			else 
+				return connection.InsertOrReplaceAsync(entry);
+		}
+
+		public Task DeleteEntryAsync (DiaryEntry entry)
+		{
+			return connection.DeleteAsync (entry);
+		}
+
+		public Task<List<DiaryEntry>> GetEntriesAsync(string accountName)
+		{
+			return connection.Table < DiaryEntry> ().Where (d => d.AccountName == accountName).ToListAsync ();
+		}
+
+		public Task<DiaryEntry> GetEntry(int id)
+		{
+			return connection.Table<DiaryEntry> ().Where (d => d.Id == id).FirstOrDefaultAsync ();
+		}
+	}
+}
