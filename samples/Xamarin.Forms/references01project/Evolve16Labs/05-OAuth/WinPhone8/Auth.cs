4f44265d88fe57f93fada879d47bd45f7fcfe7ef
Miljenko Cvjetko <mcvjetko@holisticware.net>
2016-06-27 10:46:51 +0200

    Evovle16 OAuth sample

---
+++ samples/Xamarin.Forms/references01project/Evolve16Labs/05-OAuth/WinPhone8/Auth.cs
@@ -0,0 +1,20 @@
+﻿using Xamarin.Auth;
+using Xamarin.Auth.Presenters;
+
+namespace Xamarin.Auth
+{
+	public static class Auth
+	{
+        internal static System.Windows.Controls.Page Page { get; set; }
+
+        public static void Init(System.Windows.Controls.Page page)
+        {
+            Auth.Page = page;
+
+            OAuthLoginPresenter.PlatformLogin = (authenticator) => {
+				var oauthLogin = new PlatformOAuthLoginPresenter ();
+				oauthLogin.Login (authenticator);
+			};
+		}
+	}
+}
---
+++ samples/Xamarin.Forms/references01project/Evolve16Labs/05-OAuth/iOS/OAuthPlatformPresenter.iOS.cs
@@ -0,0 +1,21 @@
+﻿namespace Xamarin.Auth
+{
+	public class PlatformOAuthLoginPresenter
+	{
+		UIKit.UIViewController rootViewController;
+
+		public void Login (Authenticator authenticator)
+		{
+			authenticator.Completed += AuthenticatorCompleted;
+
+			rootViewController = UIKit.UIApplication.SharedApplication.KeyWindow.RootViewController;
+			rootViewController.PresentViewController (authenticator.GetUI(), true, null);
+		}
+
+		void AuthenticatorCompleted (object sender, AuthenticatorCompletedEventArgs e)
+		{
+			rootViewController.DismissViewController (true, null);
+			((Authenticator)sender).Completed -= AuthenticatorCompleted;
+		}
+	}
+}
