eb90f414b8d1cc0e1a7de2070ed6440d47662b80
Alexander Marek <gentledepp@gmail.com>
2016-12-09 16:51:17 +0100

    Switched to original SharpSerializer package

--- nuget/package.ps1
+++ nuget/package.ps1
@@ -1,10 +1,10 @@
 $rootPath = Split-Path -Path $MyInvocation.MyCommand.Path
+$targetNugetExe = "$rootPath\nuget.exe"
 
-if(-not (Test-Path("$rootPath\nuget.exe"))){
+if(-not (Test-Path($targetNugetExe))){
 	$sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
-	$targetNugetExe = "$rootPath\nuget.exe"
 	Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
-	Set-Alias nuget $targetNugetExe -Scope Global -Verbose
 }
+Set-Alias nuget $targetNugetExe -Scope Global -Verbose
 
 nuget pack Xamarin.Auth.nuspec
