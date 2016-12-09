b5c35c749cc9a9f614371cf23692306625625de3
Alexander Marek <gentledepp@gmail.com>
2016-12-09 11:57:34 +0100

    Pixed minor project.json problem

--- samples/Xamarin.Auth.Sample.Android/MainActivity.cs
+++ samples/Xamarin.Auth.Sample.Android/MainActivity.cs
@@ -13,7 +13,7 @@ namespace Xamarin.Auth.Sample.Android
         void LoginToFacebook(bool allowCancel)
         {
             var auth = new OAuth2Authenticator(
-                clientId: "App ID from https://developers.facebook.com/apps",
+                clientId: Constants.KEY,
                 scope: "",
                 authorizeUrl: new Uri("https://m.facebook.com/dialog/oauth/"),
                 redirectUrl: new Uri("http://www.facebook.com/connect/login_success.html"));
