b9447beac8414833ebc7984337027d75fc89518a
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-04-12 18:19:21 +0200

    AssemblyInfo updates Xamarin.Forms nuget

--- source/Xamarin.Auth.LinkSource/AssemblyInfo.cs
+++ source/Xamarin.Auth.LinkSource/AssemblyInfo.cs
@@ -22,9 +22,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("Xamarin.Auth")]
 [assembly: AssemblyDescription("Cross platform authentication library")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Xamarin Inc.")]
+[assembly: AssemblyCompany("Microsoft / Xamarin Inc.")]
 [assembly: AssemblyProduct("Xamarin.Auth")]
-[assembly: AssemblyCopyright("2012-2016 Xamarin Inc.")]
+[assembly: AssemblyCopyright("2012-2017 Microsoft / Xamarin Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
