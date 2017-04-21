c7a4aeb2661b90510767162e5bad84b14fcde539
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-04-21 21:46:24 +0200

    AssemblyInfo.cs update to 1.5.0.0

--- source/XamarinForms/Xamarin.Auth.Forms/Properties/AssemblyInfo.cs
+++ source/XamarinForms/Xamarin.Auth.Forms/Properties/AssemblyInfo.cs
@@ -6,13 +6,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ClassLibrary1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Xamarin.Auth.XamarinForms")]
+[assembly: AssemblyDescription("Xamarin.Auth Xamarin.Forms support")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Xamarin / Mirosoft")]
+[assembly: AssemblyProduct("Xamarin.Auth.XamarinForms")]
+[assembly: AssemblyCopyright("Copyright ©  2014-2017")]
+[assembly: AssemblyTrademark("Xamarin/Microsoft Xamarin.Auth")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
