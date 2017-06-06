9b16ac02ae0a70be2c6279f86a3013a01779090b
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-06-06 20:00:51 +0200

    NatvieUIAutneticator for Facebook added, Google updated

---
+++ source/Xamarin.Auth.LinkSource/Providers/Facebook/NativeUIAuthenticator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace Xamarin.Auth.Providers.Facebook
+{
+    public partial class NativeUIAuthenticator : OAuth2Authenticator
+    {
+        public NativeUIAuthenticator
+                    (
+                        string client_id,
+                        string scope,
+                        GetUsernameAsyncFunc usernamer_getter_async
+                    )
+            : base
+                (
+                    clientId: client_id,
+                    authorizeUrl: new Uri("https://www.facebook.com/v2.9/dialog/oauth"),
+                    redirectUrl: new Uri($"fb{client_id}://authorize"),
+                    scope: scope,
+                    getUsernameAsync: usernamer_getter_async,
+                    isUsingNativeUI: true
+                )
+        {
+        }
+    }
+}
