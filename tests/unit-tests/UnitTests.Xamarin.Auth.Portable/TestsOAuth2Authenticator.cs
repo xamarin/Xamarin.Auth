487db7189aaccce2b8425b0525525317b41dbf66
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-09-23 16:19:11 +0200

    tests initial version

---
+++ tests/unit-tests/UnitTests.Xamarin.Auth.Portable/TestsOAuth2Authenticator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace UnitTests.Xamarin.Auth
+{
+    [TestFixture()]
+    public class TestsOAuth2Authenticator
+    {
+        [Test()]
+        public void TestCase_Constructors()
+        {
+            
+            return;
+        }
+    }
+}
