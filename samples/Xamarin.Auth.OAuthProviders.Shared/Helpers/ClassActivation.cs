c46c53dd9a3519a775df9f081fdc1484c0b5ec9e
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2017-10-03 20:36:37 +0200

    class activation utilities

---
+++ samples/Xamarin.Auth.OAuthProviders.Shared/Helpers/ClassActivation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Xamarin.Auth.OAuthProviders
+{
+    public class ClassActivation
+    {
+        static System.Reflection.Assembly assembly = null;
+
+        public static object CreateInstance(string class_name)
+        {
+            assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+        	var type = assembly.GetTypes().First(t => t.Name == className);
+
+        	return Activator.CreateInstance(type);           
+        }
+
+        private static System.Collections.Generic.IEnumerable<Type> GetDerivedTypesFor(Type baseType)
+        {
+        	assembly = System.Reflection.Assembly.GetExecutingAssembly();
+
+        	return assembly.GetTypes()
+        		.Where(baseType.IsAssignableFrom)
+        		.Where(t => baseType != t);
+        }
+    }
+}
