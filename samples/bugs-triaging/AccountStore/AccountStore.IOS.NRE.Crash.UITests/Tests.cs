66b24180caf6d19d0dbb6beb2233f39b68381aca
Miljenko Cvjetko moljac <mcvjetko@holisticware.net>
2019-06-26 23:23:07 +0200

    AccountStore sample iOS NRE

---
+++ samples/bugs-triaging/AccountStore/AccountStore.IOS.NRE.Crash.UITests/Tests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.iOS;
+using Xamarin.UITest.Queries;
+
+namespace AccountStore.IOS.NRE.Crash.UITests
+{
+    [TestFixture]
+    public class Tests
+    {
+        iOSApp app;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            // TODO: If the iOS app being tested is included in the solution then open
+            // the Unit Tests window, right click Test Apps, select Add App Project
+            // and select the app projects that should be tested.
+            //
+            // The iOS project should have the Xamarin.TestCloud.Agent NuGet package
+            // installed. To start the Test Cloud Agent the following code should be
+            // added to the FinishedLaunching method of the AppDelegate:
+            //
+            app = ConfigureApp
+                .iOS
+                // TODO: Update this path to point to your iOS app and uncomment the
+                // code if the app is not included in the solution.
+                //.AppBundle ("../../../iOS/bin/iPhoneSimulator/Debug/UITest.iOS.app")
+                .StartApp();
+        }
+
+        [Test]
+        public void AppDidLaunch()
+        {
+            app.Screenshot("App launched");
+        }
+    }
+}
